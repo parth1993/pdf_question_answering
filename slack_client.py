2354060aa24e321a2add4c37a26692f13e288b57
parth1993 <sharmaparth17@gmail.com>
2024-05-25 17:33:32 +0530

    remove commented code

--- slack_client.py
+++ slack_client.py
@@ -20,21 +20,3 @@ def post_answers_to_slack(channel, answers):
         return response
     except SlackApiError as e:
         print(f"Error posting to Slack: {e.response['error']}")
-
-
-# from slack_sdk import WebClient
-# from slack_sdk.errors import SlackApiError
-# import os
-
-# class SlackClient:
-#     def __init__(self):
-#         self.client = WebClient(token=os.getenv('SLACK_API_TOKEN'))
-
-#     def post_message(self, channel, text):
-#         try:
-#             response = self.client.chat_postMessage(
-#                 channel=channel,
-#                 text=text
-#             )
-#         except SlackApiError as e:
-#             print(f"Error posting message: {e.response['error']}")
